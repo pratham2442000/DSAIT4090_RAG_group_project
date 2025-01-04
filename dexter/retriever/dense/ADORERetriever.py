0479bc9b37439156283a2fa3a5a923626673ac90
Henry Page <h.page@student.tudelft.nl>
2025-01-04 20:05:35 +0100

    [ADORE Implementation], fix oopsie 2

--- dexter/retriever/dense/ADORERetriever.py
+++ dexter/retriever/dense/ADORERetriever.py
@@ -1,9 +1,13 @@
 from typing import Dict, List, Union, Tuple, Any
 
+import os
+import joblib
+import math
+
 from torch import Tensor
 from torch.optim import AdamW
 import torch
-from transformers import get_scheduler
+from transformers import get_scheduler, AutoModel
 
 import numpy as np
 from tqdm import tqdm
@@ -54,13 +58,35 @@ def compute_loss_for_query_and_hard_negatives(q_idx, relevant_doc_idxs, all_hard
 
             loss += float((switched_rr - orig_rr) * l_r)
 
-    return loss / (len(relevant_doc_idxs) * len(relevant_doc_idxs_set))
+    return loss / (len(relevant_doc_idxs) * len(all_hard_negative_idxs))
 
 
 class ADORERetriever(HfRetriever):
-    def __init__(self,config=DenseHyperParams) -> None:
+    def __init__(self,config: DenseHyperParams) -> None:
         super().__init__(config)
         self.passage_embeddings: Union[List[Tensor], np.ndarray, Tensor] = None
+
+    def encode_corpus_for_training(self, corpus: List[Evidence]):
+
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+        if self.passage_embeddings is None:
+            print("BUILDING PASSAGE EMBEDDINGS FOR ADORE TRAINING - THIS MIGHT TAKE A WHILE")
+            directory = "indices/corpus"
+            filename = os.path.join(directory, "index.joblib")
+            npy_filename = os.path.join(directory, "npy_index.npy")
+
+            # Create the directory if it doesn't exist
+            os.makedirs(directory, exist_ok=True)
+            if os.path.exists(filename):
+                print("Loaded existing passage index")
+                self.passage_embeddings = torch.tensor(joblib.load(filename), device=device)
+            else:
+                self.passage_embeddings = self.encode_corpus(corpus)
+                joblib.dump(self.passage_embeddings, filename)
+                np.save(npy_filename, self.passage_embeddings.cpu().numpy())
+
+        print("DONE BUILDING CORPUS")
 
     def train(self, queries: List[Question],
               corpus: List[Evidence],
@@ -69,31 +95,31 @@ class ADORERetriever(HfRetriever):
               n_epochs: int
               ) -> None:
 
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-        if self.passage_embeddings is None:
-            print("BUILDING PASSAGE EMBEDDINGS FOR ADORE TRAINING - THIS MIGHT TAKE A WHILE")
-            self.passage_embeddings = self.encode_corpus(corpus)
+        self.encode_corpus_for_training(corpus)
+
+        if not isinstance(self.passage_embeddings, torch.Tensor):
+            self.passage_embeddings = torch.tensor(self.passage_embeddings, device=device)
         else:
-            print("USING EXISTING PASSAGE EMBEDDINGS FOR ADORE TRAINING")
+            self.passage_embeddings = self.passage_embeddings.to(device)
 
         relevant_docs_raw_idxs = dict() # maps queries to doc positions (NOT DOC IDS)!
         for query in queries:
             relevant_docs_raw_idxs[query.id()] = []
             for raw_doc_idx, doc in enumerate(corpus):
-                if qrels[query.id()][doc.id()] == 1:
+                if doc.id() in qrels[query.id()]:
                     relevant_docs_raw_idxs[query.id()].append(raw_doc_idx)
 
-        num_training_steps = n_epochs * (len(queries) / self.batch_size)
+        num_training_steps = n_epochs * int(math.ceil(len(queries) / self.batch_size))
         optimizer = AdamW(self.question_encoder.parameters(), lr=self.config.learning_rate)
         lr_scheduler = get_scheduler(
             name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
 
-        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         self.question_encoder.to(device)
         self.question_encoder.train()
 
         progress_bar = tqdm(range(num_training_steps))
-
 
         for epoch in range(n_epochs):
             for i in range(0, len(queries), self.batch_size):
@@ -127,3 +153,13 @@ class ADORERetriever(HfRetriever):
                 lr_scheduler.step()
                 optimizer.zero_grad()
                 progress_bar.update(1)
+
+        # take it out of training mode
+        self.question_encoder.eval()
+
+    def save_query_encoder(self, save_directory: str) -> None:
+        self.question_encoder.eval()
+        self.question_encoder.save_pretrained(save_directory)
+
+    def load_query_encoder(self, load_directory: str) -> None:
+        self.question_encoder = AutoModel.from_pretrained(load_directory)
