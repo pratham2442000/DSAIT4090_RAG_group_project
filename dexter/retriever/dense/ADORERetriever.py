e31edcc7b251cc0d06e85fe0e4205bac38cefec8
Henry Page <h.page@student.tudelft.nl>
2025-01-05 19:07:20 +0100

    [ADORE Implementation] comments

--- dexter/retriever/dense/ADORERetriever.py
+++ dexter/retriever/dense/ADORERetriever.py
@@ -23,17 +23,31 @@ similarity_metric = CosineSimilarity()
 def calculate_rr(all_doc_idxs, relevant_doc_idxs_set):
     rr = 0
     for rank, doc_idx in enumerate(all_doc_idxs, start=1):
+        # If a relevant doc is not in the first 20 then we punish the model severely.
+        if rank > 20:
+            break
+
         if doc_idx in relevant_doc_idxs_set:
             rr = 1 / rank
             break
     return rr
 
 def compute_loss_for_query_and_hard_negatives(q_idx, relevant_doc_idxs, all_hard_negative_idxs, similarity_scores):
+    """
+    Computes the loss for 1 query, it considers all pairs of relevant docs and hard negatives and computes the loss as specified in the paper.
+    We use RR. It averages across docs to make MRR
+    Args:
+        q_idx: The raw q_idx which corresponds to the row in the similarity_scores tensor
+        relevant_doc_idxs: The list of relevant doc indexes which corresponds to the column in similarity_scores tensor
+        all_hard_negative_idxs:  The list of dynamic hard negative indexes sampled at the current step which corresponds to the column in similarity_scores tensor
+        similarity_scores:  The tensor which has size |Batch size of queries| x |# passages in corpus| tensor
+
+    Returns: the pairwise loss as described above.
+    """
     relevant_doc_idxs_set = set(relevant_doc_idxs)
 
-    merged_doc_idxs = relevant_doc_idxs + all_hard_negative_idxs
-
-    merged_doc_idxs = sorted(merged_doc_idxs, key=lambda idx: similarity_scores[q_idx, idx], reverse=True)
+    # sort descendingly in order of similarity scores
+    merged_doc_idxs = sorted(relevant_doc_idxs + all_hard_negative_idxs, key=lambda idx: float(similarity_scores[q_idx, idx]), reverse=True)
 
     idxs_to_pos = dict()
 
@@ -66,7 +80,16 @@ class ADORERetriever(HfRetriever):
         super().__init__(config)
         self.passage_embeddings: Union[List[Tensor], np.ndarray, Tensor] = None
 
-    def encode_corpus_for_training(self, corpus: List[Evidence]):
+    def encode_corpus_for_training(self, corpus: List[Evidence]) -> None:
+        """
+        Encodes the corpus and persists it as 2 separate files, one time as a tensor and one time as a npy array.
+        TRAIN() WILL CALL THIS FUNCTION
+        IF THE CORPUS ALREADY EXISTS AS AN INDEX IT WILL USE THAT
+        Args:
+            corpus: The corpus to encode.
+
+        Returns: None
+        """
 
         device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
@@ -94,17 +117,33 @@ class ADORERetriever(HfRetriever):
               top_k: int,
               n_epochs: int
               ) -> None:
+        """
+        Begins the training process for the ADORE retriever. It will automatically call the encoder_corpus method
+        but will use the pre-existing corpus index if it is present. Additionally one can choose just to encode the corpus
+        by calling only that method. See the documentation above.
+        Args:
+            queries: The list of queries
+            corpus: The list of Evidences objects
+            qrels: The relationships between questions and evidence.
+            top_k: how many passages we should consider at each retrieval, gets the top k
+            n_epochs: Number of epochs to train the model
+        Returns: None
+        """
 
         device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
+        # This method will encode the corpus if the index is not present, otherwise it will use the index
         self.encode_corpus_for_training(corpus)
 
+        # sanity check
         if not isinstance(self.passage_embeddings, torch.Tensor):
             self.passage_embeddings = torch.tensor(self.passage_embeddings, device=device)
         else:
             self.passage_embeddings = self.passage_embeddings.to(device)
 
-        relevant_docs_raw_idxs = dict() # maps queries to doc positions (NOT DOC IDS)!
+        # This takes roughly 2-3 minutes on the corpus since it's CPU bound.
+        # It maps queries to doc positions (NOT DOC IDS)!
+        relevant_docs_raw_idxs = dict() #
         for query in queries:
             relevant_docs_raw_idxs[query.id()] = []
             for raw_doc_idx, doc in enumerate(corpus):
@@ -126,16 +165,17 @@ class ADORERetriever(HfRetriever):
                 cur_queries = queries[i : i + self.batch_size]
                 query_embeddings = self.encode_queries(cur_queries)
 
+                # TENSOR SHAPE IS |Q| * |C|
+                similarity_scores = similarity_metric.evaluate(query_embeddings, self.passage_embeddings)
 
-                similarity_scores = similarity_metric.evaluate(query_embeddings, self.passage_embeddings) # TENSOR SHAPE IS |Q| * |C|
-
+                # We take +20 since relevant docs might be in the topk. We always want the topk to be hard negatives only.
                 top_k_values, top_k_idxs =torch.topk(similarity_scores, min(top_k + 20, len(similarity_scores[1])), dim=1, largest=True, sorted=True)
                 top_k_idxs = top_k_idxs.cpu().numpy()
                 # top_k_values = top_k_values.cpu().numpy()
                 batch_total_loss = 0
 
                 for q_idx, query in enumerate(cur_queries):
-
+                    # For each query in the batch, retrieve the top-k hard negatives
                     all_hard_negative_idxs= [
                         doc_raw_idx
                         for doc_raw_idx in top_k_idxs[q_idx]
@@ -143,6 +183,7 @@ class ADORERetriever(HfRetriever):
                     ]
 
                     assert len(all_hard_negative_idxs) >= top_k
+                    # we take the top_k and eliminate the +20 part, this will only have hard-negatives
                     all_hard_negative_idxs = all_hard_negative_idxs[:top_k]
 
                     batch_total_loss += compute_loss_for_query_and_hard_negatives(q_idx, relevant_docs_raw_idxs[query.id()], all_hard_negative_idxs, similarity_scores)
@@ -157,9 +198,23 @@ class ADORERetriever(HfRetriever):
         # take it out of training mode
         self.question_encoder.eval()
 
-    def save_query_encoder(self, save_directory: str) -> None:
+    def save_query_encoder(self, save_directory : str) -> None:
+        """
+        Save fine-tuned model
+        Args:
+            save_directory: The directory where we want to save it, it has to exist, otherwise exception
+
+        Returns: None
+        """
         self.question_encoder.eval()
         self.question_encoder.save_pretrained(save_directory)
 
     def load_query_encoder(self, load_directory: str) -> None:
+        """
+        Load fine-tuned model
+        Args:
+            load_directory: The directory where it is saved, it has to exist, otherwise exception
+
+        Returns: None
+        """
         self.question_encoder = AutoModel.from_pretrained(load_directory)
